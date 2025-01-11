1de34cfe5dde44dd080a2eae6233ef211216cf61
Lui Jangulashvili <luijangulashvili@gmail.com>
2025-01-12 02:29:50 +0400

    finished backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from './config/db.js';
+import { connectDB } from './config/db.js'; 
+
+import productRoutes from "./routes/product.route.js"
 
 dotenv.config(); 
 
 const app = express();
- 
-app.get("/", (req, res) => {});
 
-app.listen(5000, () => {
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/products", productRoutes);
+ 
+app.listen(PORT, () => {
     connectDB();
-    console.log("server started at http://localhost:5000");
+    console.log("server started at http://localhost:" + PORT);
 });
